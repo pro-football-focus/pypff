4dde965bf44e9fc636b41359aed58353b56f9dc6
apschram <84464847+apschram@users.noreply.github.com>
2022-08-18 16:22:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 18 16:18:30 2022
+
+@author: apschram
+"""
+from distutils.core import setup
+
+setup(name='PyPFF',
+      version='0.1',
+      description='PFF FC Software Development Kit for Python',
+      author='Alexander Schram',
+      author_email='alexander.schram@pff.com',
+      url='https://github.com/pro-football-focus/pypff/',
+     )
