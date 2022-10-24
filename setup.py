4df8ebafe3b5e115830879a7267c1747771769f4
apschram <84464847+apschram@users.noreply.github.com>
2022-10-24 15:25:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Created on Thu Aug 18 16:18:30 2022
 
 @author: apschram
 """
-from setuptools import setup
+from setuptools import setup,find_namespace_packages
 
 setup(name='PyPFF',
       version='0.2',
@@ -13,9 +13,10 @@ setup(name='PyPFF',
       author='PFF FC',
       author_email='fchelp@pff.com',
       url='https://github.com/pro-football-focus/pypff/',
-      packages=['pypff'],
+      # packages=['pypff'],
       py_modules=['pff','normalize'],
       install_requires=['pandas','requests','pyhumps'],
-      # package_dir={'':'pypff'},
-      package_data={'norm':['*.pickle']}
+      packages=find_namespace_packages(where='pypff'),
+      package_dir={"": "pypff"},
+      package_data={"norm": ["*.pickle"]}
      )
