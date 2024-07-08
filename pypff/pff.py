9e71d1ee75bb269265bce01cb524431c9ec4f91b
apschram <alexander.schram@pff.com>
2024-07-08 15:26:44 +0200

    Update pff.py
    Fix second_nationality in get_player function

--- pypff/pff.py
+++ pypff/pff.py
@@ -247,6 +247,7 @@ def get_player(url, key, player_id):
     
     try:
         player_data = response.json()['data']['player']
+        second_nationality = player_data['secondNationality']['country'] if player_data['secondNationality'] else None
         player_record = {
             'player_id': player_data['id'],
             'first_name': player_data['firstName'],
@@ -256,7 +257,7 @@ def get_player(url, key, player_id):
             'nickname': player_data['nickname'],
             'position_group': player_data['positionGroupType'],
             'nationality': player_data['nationality']['country'],
-            'second_nationality': player_data['secondNationality']['country'],
+            'second_nationality': second_nationality,
             'transfermarkt_id': player_data['transfermarktPlayerId'],
             'rosters': player_data['rosters']  # This will remain a nested list of dicts
         }
